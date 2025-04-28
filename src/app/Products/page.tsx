752ed0ae8dc3574eb8b8ae629d7ff8c9ab02b00d
Pablo <Pablo@gmail.com>
2025-04-28 10:17:10 -0300

    tela descrição prod

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -6,12 +6,15 @@ import { Product } from "@/types/Interface";
 import { useState, useEffect } from "react";
 import { useSearchParams } from 'next/navigation'; // Importa useSearchParams
 import { FaRegStar } from "react-icons/fa";
+import { IoAdd } from "react-icons/io5";
+import { SlSizeActual } from "react-icons/sl";
 
 const Products = () => {
   const searchParams = useSearchParams(); // Obtém os parâmetros da URL
   const id = searchParams.get('id'); // Pega o ID da query string
   const [loading, setLoading] = useState(false);
   const [product, setProduct] = useState<Product | null>(null); // Altera para um objeto do tipo Product ou null
+  const [quantity, setQuantity] = useState(0); // Estado para armazenar a quantidade do estoque
 
   useEffect(() => {
     const fetchProductById = async () => {
@@ -19,9 +22,24 @@ const Products = () => {
       try {
         console.log(id);
         const res = await api.get(`/Produtos/ID/${id}`); // Use o ID obtido da URL
-        const data: Product = res.data[0]
+        const data: Product = res.data[0];
         setProduct(data); // Define o produto recebido
-        console.log(data)
+        console.log(data);
+      } catch (error) {
+        console.error("Erro na requisição:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const fetchStockProductById = async () => {
+      setLoading(true);
+      try {
+        console.log(id);
+        const res = await api.get(`/Estoques/${id}`); // Use o ID obtido da URL
+        const stockData = res.data; // Supondo que a resposta contenha a quantidade
+        setQuantity(stockData.quantidade); // Ajuste conforme a estrutura do retorno
+        console.log(stockData);
       } catch (error) {
         console.error("Erro na requisição:", error);
       } finally {
@@ -31,6 +49,7 @@ const Products = () => {
 
     if (id) {
       fetchProductById(); // Chama a função para buscar o produto específico se o ID estiver disponível
+      fetchStockProductById(); // Chama a função para buscar o estoque do produto
     }
   }, [id]);
 
@@ -39,7 +58,6 @@ const Products = () => {
       <MainBar />
       <main className={styles.main}>
         <div className={styles.container}>
-          {loading && <p>Carregando...</p>}
           {product ? ( // Verifica se product não é null
             <>
               <img src={product.img} alt="" />
@@ -61,8 +79,23 @@ const Products = () => {
                   <p>{product.qAval} Reviews</p>
                 </div>
                 <p>{product.descricao}</p>
+                <div className={styles.buy}>
+                  <div className={styles.info}>
+                    <div>
+                      <input type="text" placeholder="Selecione o Tamanho" />
+                      <div><SlSizeActual className={styles.i} /></div>
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Selecione a Quantidade" />
+                      <div><IoAdd className={styles.i} /></div>
+                    </div>
+                  </div>
+                  <div className={styles.btn}>
+                    <button>Adicionar ao Carinho</button>
+                    <button>Comprar Agora</button>
+                  </div>
+                </div>
               </div>
-
             </>
           ) : (
             <p>Carregando</p> // Mensagem caso o produto não esteja disponível
